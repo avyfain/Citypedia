87579b28cf96221ce55d5029b431c75abfb97d45
avyfain <avyfain@gmail.com>
2014-11-24 01:49:50 -0600

    stuff

--- linkrank.py
+++ linkrank.py
@@ -3,7 +3,9 @@ import requests
 import threading
 from collections import Counter, defaultdict
 import math
-import json
+# import json
+import operator
+import urllib
 
 def get_idfs(dicts):
     idfs = defaultdict(float)
@@ -43,6 +45,7 @@ def links_for_city(city):
                              and not a.attrs['href'].startswith('/wiki/Wikipedia:')\
                              and not a.attrs['href'].startswith('/wiki/Special:')\
                              and not a.attrs['href'].startswith('/wiki/Template:')\
+                             and not a.attrs['href'].startswith('/wiki/Template_talk:')\
                              and not a.attrs['href'].startswith('/wiki/International_Standard_Book_Number'):
             name = a.attrs['href']
             name = name.replace('/wiki/', '')
@@ -95,7 +98,42 @@ def main():
         for k,v in out_dict[c].items():
             out_dict[c][k] = out_dict[c][k]*corpus[k]
 
-    print json.dumps(out_dict, ensure_ascii=False)
+    # print json.dumps(out_dict, ensure_ascii=False)
+
+    bin_breaks = {'Chicago':        [0.004015172595221041, 0.0020075862976105204, 0.0014292407151658435, 0.0008027006705568285,-1],\
+                  'London':         [0.003379342946188495, 0.0016896714730942476, 0.001006416505027073, 0.0005324253825028401,-1],\
+                  'New_York_City':  [0.005967983904967859, 0.0019893279683226195, 0.0014162422464871957, 0.0009620825570832646,-1],\
+                  'Paris':          [0.003920313120466328, 0.001960156560233164, 0.0016778155498217812, 0.0007010761311666402,-1],\
+                  'Tokyo':          [0.009380476326262021, 0.0031268254420873404, 0.0015687388450773532, 0.0010894672770911421,-1]};
+
+    colors = ['#243392', '#2962ff', '#3d9dfd', '#8fc6ff', '#e2f0ff'];
+
+    html_top = \
+    '''
+    <!DOCTYPE html>
+    <html>
+    <body>
+    '''
+
+    html_bottom = \
+    '''
+    </body>
+    </html>
+    '''
+
+    for city, links in out_dict.items():
+      Html_file= open(city + ".html","w")
+      Html_file.write(html_top)
+      sorted_links = sorted(links.items(), key=operator.itemgetter(1), reverse=True)
+      color_val = 0
+      for link in sorted_links:
+        if link[1] < bin_breaks[city][color_val]:
+          color_val+=1
+        the_vars = (colors[color_val], urllib.unquote(link[0]).replace('_', ' '))
+        html_str = '''<p style="text-align: center;"><a style="background: %s; color: white; padding: 2px;">%s</a></p>\n''' % the_vars
+        Html_file.write(html_str)
+      Html_file.write(html_bottom)
+      Html_file.close()
 
 if __name__ == '__main__':
     main()
