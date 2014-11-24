92ab6d5ed4a3b0b9e7dcbcd7d610c5b765c4b64a
Leon Sasson <leonsassonha@gmail.com>
2014-11-24 01:16:28 -0800

    png generator

---
+++ layout_html_to_png.sh
@@ -0,0 +1,11 @@
+# Modifica cada linea a tu path, y copy/paste a console
+
+/Users/leonsas/projects/temp/capturejs/capture.js --uri Paris.html             --viewportsize 9000x1400             --zoomfactor 3                 --output 'paris_layout.png'
+
+/Users/leonsas/projects/temp/capturejs/capture.js --uri London.html            --viewportsize 9000x1400             --zoomfactor 3                 --output 'london_layout.png'
+
+/Users/leonsas/projects/temp/capturejs/capture.js --uri Tokyo.html             --viewportsize 9000x1400             --zoomfactor 3                 --output 'tokyo_layout.png'
+
+/Users/leonsas/projects/temp/capturejs/capture.js --uri New_York_City.html             --viewportsize 9000x1400             --zoomfactor 3                 --output 'newyork_layout.png'
+
+/Users/leonsas/projects/temp/capturejs/capture.js --uri chicago.html             --viewportsize 9000x1400             --zoomfactor 3                 --output 'chicago_layout.png'
